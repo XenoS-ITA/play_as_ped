a249217bb8b63fa155978b41525476d7eff3e8a0
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-02-26 02:33:29 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,14 +1,13 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterServerCallback('play_as_ped:GetGroup', function(source, cb)
-    TriggerEvent('es:getPlayerFromId', source, function(user)
-        cb(user.getGroup())
-    end)
-end)
-
-ESX.RegisterServerCallback('play_as_ped:GetIdentifier', function(source, cb)
+ESX.RegisterServerCallback('play_as_ped:getData', function(source, cb)
     local xPlayer = ESX.GetPlayerFromId(source)
+    local data = {
+        identifier    = xPlayer.identifier
+        group   = xPlayer.getGroup(),
 
-    cb(xPlayer.identifier)
+    }
+    
+    cb(data)
 end)
