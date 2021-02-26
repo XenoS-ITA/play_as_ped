7e7f156e572937fd36d3ef841394a8f9677f564d
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-02-26 02:41:09 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,13 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'A script to change your character into a ped'
+
+version '1.0.1'
 
 server_script 'server.lua'
+
 client_script 'client.lua'
 
 dependencies {
