d69816ac9506fa8376d0b71014759b5e5b590e2e
Xenos.exe <55803068+XenoS-ITA@users.noreply.github.com>
2021-03-03 10:19:46 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,7 +4,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 ESX.RegisterServerCallback('play_as_ped:getData', function(source, cb)
     local xPlayer = ESX.GetPlayerFromId(source)
     local data = {
-        identifier    = xPlayer.identifier
+        identifier    = xPlayer.identifier,
         group   = xPlayer.getGroup(),
 
     }
